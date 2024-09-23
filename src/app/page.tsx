98d4c4c473b77de5384a0d021caf47e4885d4802
me <me@me.me>
2024-09-23 11:39:54 +0300

    add Icon and sprite to template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import Image from 'next/image'
-import Img from '@/images/next.svg'
-
 export default function Home() {
 	return (
 		<div>
 			<h1>Home page</h1>
-			<svg>
-				<use href={`/images/sprite.svg#icon-telegram`} />
-			</svg>
 		</div>
 	)
 }
