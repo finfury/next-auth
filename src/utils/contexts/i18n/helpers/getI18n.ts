25c72af4315c624e3e81be3390fa72ae1f63463e
me <me@me.me>
2024-09-26 11:10:11 +0300

    👑 create i18n

--- src/utils/contexts/i18n/helpers/getI18n.ts
+++ src/utils/contexts/i18n/helpers/getI18n.ts
@@ -1,7 +1,11 @@
 import { createIntl } from 'react-intl'
 import { getMessagesByLocale, Locale } from './getMessagesByLocale'
+import { useContext } from 'react'
+import { I18nContext } from '../I18nProvider'
 
-export const getI18n = (locale: Locale) => createIntl({
-	locale,
-	messages: getMessagesByLocale(locale)
-})
+export const getI18n = (locale: Locale) => {
+	return createIntl({
+		locale,
+		messages: getMessagesByLocale(locale)
+	})
+}
